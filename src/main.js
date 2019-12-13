b736038d8bc6972804308538dcafd56426759152
Rita Cardoso <rita.cardoso.17@gmail.com>
2019-12-13 12:32:21 +0000

    vue e cenas

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+import BootstrapVue from 'bootstrap-vue';
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false;
 
 new Vue({
@@ -10,3 +11,4 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount("#app");
+
