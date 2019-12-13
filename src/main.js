3a47f611fa452b3d395edf8c9b139b68994cf54f
Rita Cardoso <rita.cardoso.17@gmail.com>
2019-12-13 14:56:55 +0000

    ih

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
+import "@babel/polyfill";
+import "mutationobserver-shim";
 import Vue from "vue";
+import "./plugins/bootstrap-vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import BootstrapVue from 'bootstrap-vue';
-Vue.use(BootstrapVue)
 Vue.config.productionTip = false;
 
 new Vue({
@@ -11,4 +12,3 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount("#app");
-
