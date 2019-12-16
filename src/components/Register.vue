755f9c23b7eb4c7a6669577434ddab60da317ad0
Rita Cardoso <rita.cardoso.17@gmail.com>
2019-12-16 02:59:11 +0000

    kinda começo de register

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -4,38 +4,74 @@
             <h1 >Criar Conta</h1>
             <form v-on:submit.prevent="addUser()">
                 <div class="form-group">
-                    <label for="registerName">Nome:</label>
-                    <input type="text" class="form-control" id="registerName" placeholder="Insira o seu nome" required
-                        v-model="name">
+                    <label for="txtName">Nome:</label>
+                    <input type="text" class="form-control" id="txtName" placeholder="Insira o seu nome" required  v-model="name">
                 </div>
                 <div class="form-group">
-                    <label for="registerEmail">E-mail:</label>
-                    <input type="email" class="form-control" id="registerEmail" aria-describedby="emailHelp"
-                        placeholder="Enter email" required v-model="email">
-                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
-                        else.</small>
+                    <label for="emailTxt">E-mail:</label>
+                    <input type="email" class="form-control" id="emailTxt" aria-describedby="emailHelp" placeholder="Insira o seu email" required v-model="email">
                 </div>
                 <div class="form-group">
-                    <label for="registerPass">Password:</label>
-                    <input type="password" class="form-control" id="registerPass" placeholder="Password" required
-                        v-model="password">
+                    <label for="passTxt">Password:</label>
+                    <input type="password" class="form-control" id="passTxt" placeholder="Password" required v-model="password">
                 </div>
                 <div class="form-group">
-                    <label for="registerConfPass">Confirmar Password:</label>
-                    <input type="password" class="form-control" id="registerConfPass"
-                        placeholder="Confirme sua Password" required v-model="confPassword">
+                    <label for="confirmarPassTxt">Confirmar Password:</label>
+                    <input type="password" class="form-control" id="confirmarPassTxt"
+                        placeholder="Confirme sua Password" required v-model="confPass">
                 </div>
                 <button type="submit" class="btn btn-primary  float-right">Criar Conta</button>
-                <button type="button" class="btn btn-link float-right">
+                <!-- <button type="button" class="btn btn-link float-right">
                     <router-link to="/login">Login</router-link>
-                </button>
+                </button> -->
             </form>
         </div> 
-    </div>
+    </div> 
 </template>
 
 <script>
 export default {
-    
+    name:"Register",
+    data: ()=>({
+            id: 0,
+            name: "",
+            email: "",
+            password: "",
+            confPass: "",
+    }),  
+    created: function () {
+            window.addEventListener('unload', this.saveStorage)
+            if (localStorage.getItem("users")) {
+                this.$store.state.users = JSON.parse(localStorage.getItem("users"))
+            }
+            if (localStorage.getItem("loggedUser")) {
+                this.$store.state.loggedUser = JSON.parse(localStorage.getItem("loggedUser"))
+            }
+        },
+     methods: {
+        /**
+         * GET LAST ID
+         */
+        getLastId() {
+          return this.$store.getters.lastId
+        },
+        /***
+         * FUNCTION CREATED TO ADD NEW TRAVEL
+         */
+        addUser() {
+            this.$store.commit('ADD_USER',{
+                id: this.getLastId() + 1,
+                name: this.name,
+                email:this.email,
+                password:this.password,
+                confPass:this.confPass
+            
+                })
+        },
+         saveStorage() {
+                localStorage.setItem("users", JSON.stringify(this.$store.state.users))
+                localStorage.setItem("loggedUser", JSON.stringify(this.$store.state.loggedUser))
+            }
+    }
 }
-</script>
+</script> 
--- src/store/index.js
+++ src/store/index.js
@@ -5,13 +5,35 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    user: [],
+    users: [],
+    logged:false,
     loggedUser:''
   },
 
   mutations: {
-    register(){
-      
+    ADD_USER: (state,payload) => {
+      //SE ENCONTRAR ALGUM EMAIL IGUAL AO QUE ESTA A TENTAR CRIAR
+      if (!state.users.some(user => user.email === payload.email)) {
+        alert("PASSWORDS DIFERENTES")
+      }
+      if(payload.password !== payload.confPass){
+        alert("PASSWORDS DIFERENTES")
+      }
+      else {
+        state.users.push({
+          id: payload.id,
+          name: payload.name,
+          email: payload.email,
+          password: payload.password,
+        });
+        state.loggedUser.push({
+          id: payload.id,
+          name: payload.name,
+          email: payload.email,
+          password: payload.password,
+        })
+        alert("REGISTADO")
+      }
     }
   },
 
