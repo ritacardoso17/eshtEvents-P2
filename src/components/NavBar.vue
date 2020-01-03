65e2478f54a6f40005c1daf133f2d5eb3df0910e
Marianne Vaz <9180315@esmad.ipp.pt>
2020-01-03 01:32:34 +0000

    footer e login FIM

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -12,16 +12,36 @@
         </li>
       </ul>
 
-      <a class="nav-link" v-b-modal.modal-1 id="login">Iniciar Sessão<!-- moustache? --></a>
-      <b-modal id="modal-1" centered size="m" title="Iniciar Sessão" hide-footer>
+      <a class="nav-link" v-b-modal.modal-1 id="login">
+        Iniciar Sessão
+        <!-- moustache? -->
+      </a>
+      <b-modal id="modal-1" centered size="m" title="Iniciar Sessão" hide-header-close hide-footer>
+        <b-img src="../assets/noun_X_2290001.png" id="x1"></b-img>
+        <b-img src="../assets/noun_X_2290001.png" id="x2"></b-img>
         <div class="form-group">
-           <form v-on:submit.prevent="login()">
-          <label for="txtEmailLogin">E-mail:</label>
-          <input  type="email" class="form-control" id="txtEmailLogin" aria-describedby="emailHelpId" placeholder="Insira o seu e-mail" v-model="email" required/>
-          <label for="txtPasswordLogin">Palavra-Chave:</label>
-         <input type="password" class="form-control"  id="txtPasswordLogin" aria-describedby="emailHelpId" placeholder="Insira a sua palavra-chave" v-model="password" required/>
-         <b-button type="submit" class="btn btn-primary  float-right">Entrar</b-button>
-        </form>
+          <form v-on:submit.prevent="login()">
+            <label for="txtEmailLogin" class="emailLogin">E-mail:</label>
+            <input
+              type="email"
+              class="form-control"
+              id="txtEmailLogin"
+              placeholder="Insira o seu e-mail"
+              v-model="email"
+              required
+            />
+            <label for="txtPasswordLogin" class="passLogin">Palavra-Chave:</label>
+            <input
+              type="password"
+              class="form-control"
+              id="txtPasswordLogin"
+              placeholder="Insira a sua palavra-chave"
+              v-model="password"
+              required
+            />
+            <b-button type="submit" class="btn btn-primary float-right" id="btnLogin">Entrar</b-button>
+            <a href="" id="join">Ainda não estás registado? Junta-te a nós!</a>
+          </form>
         </div>
       </b-modal>
     </nav>
@@ -29,10 +49,13 @@
     <!--Navbar-->
     <nav class="navbar" id="navBar2">
       <div class="container" style="justify-content: center">
-        <router-link to="/events" id="eventsLink"> Eventos e Catering</router-link> <span> ///// </span>
-        <router-link to="/room" id="roomsLink">Espaços</router-link> <span> ///// </span>
-        <router-link to="/workshops" id="workshopsLink"> Workshops</router-link> <span> ///// </span>
-        <router-link to="/menus" id="menusLink"> Menus</router-link>
+        <router-link to="/events" id="eventsLink">Eventos e Catering</router-link>
+        <span>/////</span>
+        <router-link to="/room" id="roomsLink">Espaços</router-link>
+        <span>/////</span>
+        <router-link to="/workshops" id="workshopsLink">Workshops</router-link>
+        <span>/////</span>
+        <router-link to="/menus" id="menusLink">Menus</router-link>
       </div>
     </nav>
     <!--/.Navbar-->
@@ -41,87 +64,147 @@
 
 <script>
 export default {
-  name:"Login",
-  data:()=>({
-    email:"",
-    password:""
+  name: "Login",
+  data: () => ({
+    email: "",
+    password: ""
   }),
-  created: function () {
-            if (localStorage.getItem("users")) {
-                this.$store.state.users = JSON.parse(localStorage.getItem("users"))
-            }
-            if(localStorage.getItem("loggedUser")){
-                this.$store.state.loggedUser = JSON.parse(localStorage.getItem("loggedUser"))
-            }
-        },
-  methods:{
+  created: function() {
+    if (localStorage.getItem("users")) {
+      this.$store.state.users = JSON.parse(localStorage.getItem("users"));
+    }
+    if (localStorage.getItem("loggedUser")) {
+      this.$store.state.loggedUser = JSON.parse(
+        localStorage.getItem("loggedUser")
+      );
+    }
+  },
+  methods: {
     /**VER SE EXISTE UM USER COM O EMAIL E PASS */
-    login(){
-      this.$store.commit('LOGIN',{
-        email:this.email,
-        password:this.password
-      })
+    login() {
+      this.$store.commit("LOGIN", {
+        email: this.email,
+        password: this.password
+      });
     }
   }
-  
-}
+};
 </script>
 
 <style>
 .navbar-light {
   background-color: white !important;
 }
-
 #navBar2 {
   background-color: #232323;
   height: 40px;
 }
-
+.modal-title {
+  font-family: Channel;
+  margin-left: 130px;
+}
+.modal-header {
+  border: none;
+}
 img {
   width: 40%;
   height: auto;
 }
-
+#join{
+  font-size: 80%;
+  font-family: GeosansLight;
+  color: #000;
+}
+#x1{
+  position: relative;
+  width: 30px;
+  height: auto;
+  top: -65px;
+}
+#x2{
+  position: relative;
+  width: 30px;
+  height: auto;
+  top: -65px;
+  left: 405px;
+}
+#txtEmailLogin {
+  position: relative;
+  width: 250px;
+  left: 130px;
+  top: -43px;
+}
+#txtPasswordLogin {
+  position: relative;
+  width: 250px;
+  left: 130px;
+  top: -45px;
+}
+.emailLogin {
+  font-family: GeosansLight;
+  margin-left: 80px;
+}
+.passLogin {
+  font-family: GeosansLight;
+  margin-left: 30px;
+}
+#btnLogin {
+  position: relative;
+  width: 80px;
+  height: 30px;
+  font-size: 70%;
+  color: white;
+  white-space: nowrap;
+  padding: 2px;
+  left: -10px;
+  background-color: #232323;
+  font-family: GeosansLight;
+}
+#btnLogin:hover {
+  color: white;
+  border: 2px solid;
+  border-color: #daaa29;
+  background-color: #000;
+}
 #camp {
   width: 10%;
   height: auto;
 }
-.container{
+.container {
   padding: 20px;
   padding-bottom: 0;
   padding-top: 0;
   font-family: GeosansLight;
-
 }
-#eventsLink{
+#eventsLink {
   color: white;
   text-decoration: none;
 }
-#eventsLink:hover{
-  color: #DAAA29;
+#eventsLink:hover {
+  color: #daaa29;
 }
-#roomsLink{
+#roomsLink {
   color: white;
   text-decoration: none;
 }
-#roomsLink:hover{
-  color: #DAAA29;
+#roomsLink:hover {
+  color: #daaa29;
 }
-#workshopsLink{
+#workshopsLink {
   color: white;
   text-decoration: none;
 }
-#workshopsLink:hover{
-  color: #DAAA29;
+#workshopsLink:hover {
+  color: #daaa29;
 }
-#menusLink{
+#menusLink {
   color: white;
   text-decoration: none;
 }
-#menusLink:hover{
-  color: #DAAA29;
+#menusLink:hover {
+  color: #daaa29;
 }
-span{
+span {
   color: #232323;
 }
 </style>
