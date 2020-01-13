e387be3c1fca639ee7f7b6c0386442e7197f18a0
Marianne Vaz <9180315@esmad.ipp.pt>
2020-01-13 10:21:22 +0000

    routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,114 +1,91 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
-import Home from "../views/Home.vue";
-import InicialPage from "../views/InicialPage.vue"
-import About from "../views/About.vue";
-import Register from "../views/Register.vue";
-import Profile from "../views/Profile.vue";
-import Events from "../views/Events.vue";
-import EditProfile from "../views/EditProfile.vue";
-import RentRoom from "../views/RentRoom.vue";
-import EventsReserv from "../views/EventsReserv.vue";
-import Room from "../views/Room.vue";
-import Workshops from "../views/Workshops.vue";
-import MenuBackoffice from "../views/BackofficeMenu.vue";
-import Menu from "../views/Menu.vue";
-import UserBackoffice from "../views/userBackOffice.vue"
-import WorkshopBackoffice from "../views/workshopBackoffice.vue"
-import addWorkshop from "../views/addWorkshop.vue"
-
-
-
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
     name: "home",
-    component: Home
+    component: () => import("../views/Home.vue")
   },
   {
     path: "/addWorkshop",
     name: "addWorkshop",
-    component: addWorkshop
+    component: () => import("../views/addWorkshop.vue")
   },
   {
     path: "/InicialPage",
     name: "inicialPage",
-    component: InicialPage
+    component: () => import("../views/InicialPage.vue")
   },
   {
     path: "/eventsReserv",
     name: "eventsReserv",
-    component: EventsReserv
+    component:  () => import("../views/EventsReserv.vue")
   },
-
   {
     path: "/about",
     name: "about",
-    component: About
+    component: () => import("../views/About.vue")
   },
   {
     path: "/events",
     name: "events",
-    component: Events
+    component: () => import("../views/Events.vue")
   },
   {
     path: "/register",
     name: "register",
-    component: Register
+    component: () => import("../views/Register.vue")
   },
   {
     path: "/profile",
     name: "profile",
-    component: Profile
+    component: () => import("../views/Profile.vue")
   },
   {
     path: "/profile/editProfile",
     name: "editProfile",
-    component: EditProfile
+    component:  () => import("../views/EditProfile.vue")
   },
   {
     path: "/rentRoom",
     name: "rentRoom",
-    component: RentRoom
-  },
-  {
-    path: "/reservation/events",
-    name: "events",
-    component: EventsReserv
+    component:  () => import("../views/RentRoom.vue")
   },
   {
     path: "/room",
     name: "room",
-    component: Room
+    component:  () => import("../views/Room.vue")
   },
   {
     path: "/workshops",
     name: "workshops",
-    component: Workshops
+    component:  () => import("../views/Workshops.vue")
   },
   {
     path: "/menu",
     name: "menu",
-    component: Menu
+    component:  () => import("../views/Menu.vue")
   },
   {
     path: "/menuadmin",
     name: "menuadmin",
-    component: MenuBackoffice
+    component:  () => import("../views/BackofficeMenu.vue")
   },
   {
     path: "/usersadmin",
     name: "usersadmin",
-    component: UserBackoffice
+    component: () => import("../views/userBackOffice.vue")
   },
   {
     path: "/workshopadmin",
     name: "workshopadmin",
-    component: WorkshopBackoffice
+    component:  () => import("../views/workshopBackoffice.vue")
   }
+
+ 
 ];
 
 const router = new VueRouter({
