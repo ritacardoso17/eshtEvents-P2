3b2d331f4308fe4edddec98c93ee673d496edb45
raquelesilva <raquelesilva99@gmail.com>
2020-06-19 12:43:58 +0100

    uninstall

--- Tests/unitTest.spec.js
+++ Tests/unitTest.spec.js
@@ -1,9 +1,20 @@
-// import { shallowMount } from "@vue/test-utils";
-// import app from "@/App.vue";
+/* eslint-disable no-undef */
+import { shallowMount } from "@vue/test-utils";
+import app from "@/App.vue";
+import rents from "@/views/room.vue";
 
-// describe("App.vue", () => {
-//   it("has data", () => {
-//     const msg = "hey";
-//     expect(msg.props()).toBe(true);
-//   })
-// });
+describe("App.vue", () => {
+  it("has data", () => {
+    const msg = "hey";
+    const wrapper = shallowMount(app, {
+      propsData: { msg }
+    });
+    expect(wrapper.props()).toBe(true);
+  });
+});
+
+test("emit events when btnRent clicked", () => {
+  const btnRent = wrapper.find("#btnRent");
+  btnRent.trigger("click");
+  expect(wrapper.emitted().pressed.length).toBe(1);
+});
