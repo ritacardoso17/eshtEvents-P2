8b35320f63316328b671e3823cb0a0fe5bf7da6b
raquelesilva <raquelesilva99@gmail.com>
2020-06-19 10:59:49 +0100

    Testes init

---
+++ Tests/unitTest.spec.js
@@ -0,0 +1,9 @@
+// import { shallowMount } from "@vue/test-utils";
+// import app from "@/App.vue";
+
+// describe("App.vue", () => {
+//   it("has data", () => {
+//     const msg = "hey";
+//     expect(msg.props()).toBe(true);
+//   })
+// });
